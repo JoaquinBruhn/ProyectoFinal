d7ca6bd0991a75aa1ccf1f72473050d67f8b6ab5
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2021-08-15 16:25:45 -0300

    desafio n#5 de JavaScript

--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -1,26 +1,42 @@
-let pasword = prompt("Ingresar contraseña: (es 1234)")
-let intentos = 1
+class ropa{
+    constructor(tipo,talle,color,patron,precio){
+        this.tipo = tipo;
+        this.talle = talle;
+        this.color = color;
+        this.patron = patron;
+        this.precio = precio;
+    }
+    caracteristicas(){
+        return ("tipo de prenda: "+this.tipo+" \n"+"talle: "+this.talle+" \n"+"color/es: "+this.color+" \n"+"patron: "+this.patron+" \n"+"precio: "+this.precio)
+    }
 
-while (pasword != 1234){
-    switch (pasword){
-        case "123":
-            alert("casi papu, metele uno mas");
-            break;
-        default :
-            alert("contraseña incorrecta, intente de nuevo");
-            break
-    }
-    intentos=1+intentos;
-    if (intentos == 4){
-        alert("numero de intentos superados");
-        break;
-    }
-    pasword = prompt("Ingresar contraseña: ");
 }
 
-if (pasword == 1234){
-    alert("Bienvenido")
+const ropa1 = new ropa("pantalon", "S", "negro","ninguno", "$900");
+const ropa2 = new ropa("camisa", "M", "blanca y celeste", "rallada", "$800");
+const ropa3 = new ropa("zapatos","40","negro","ninguno","$1400");
+const ropa4 = new ropa("remera","S","naranja","ninguno","$550");
+const ropa5 = new ropa("buzo","L","marron","dibujo","$1200");
+
+let continuar
+alert("bienvenido")
+let prenda = prompt("por favor elija cual de nuestras mejores prendas quiere ver.(pantalon,camisa,zapatos,remera o buzo)")
+prenda = prenda.toLowerCase()
+if(prenda=="pantalon"){
+    alert(ropa1.caracteristicas());
 }
-else {
-    alert("Ingrese a su mail para recuperar su contraseña")
+else if(prenda=="camisa"){
+    alert(ropa2.caracteristicas());
+}
+else if(prenda=="zapatos"){
+    alert(ropa3.caracteristicas());
+}
+else if(prenda=="remera"){
+    alert(ropa4.caracteristicas());
+}
+else if(prenda=="buzo"){
+    alert(ropa5.caracteristicas());
+}
+else{
+    alert("error")
 }
