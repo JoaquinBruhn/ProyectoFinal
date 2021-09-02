5331123a1ce8de6939ce95423c2f77a9e003d8e0
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2021-09-01 21:09:42 -0300

    vercion verreta de un carrito

--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -2,11 +2,11 @@
 const inventario = []
 // constructor para maquetar los items del inventario
 class ropa{
-    constructor(tipo,talle,color,patron,precio){
-        this.tipo = tipo;
+    constructor(nombre,talle,color,imgSrc,precio){
+        this.nombre = nombre;
         this.talle = talle;
         this.color = color;
-        this.patron = patron;
+        this.imgSrc = imgSrc;
         this.precio = precio;
     }
     visualizar(){
@@ -14,53 +14,101 @@ class ropa{
     }
 }
 // items que van a pasar a formar parte del inventario
-inventario.push(new ropa("pantalon", "S", "negro","ninguno", 900))
-inventario.push(new ropa("camisa", "M", "blanca y celeste", "rallada", 800))
-inventario.push(new ropa("zapatos","40","negro","ninguno",1400))
-inventario.push(new ropa("remera","S","naranja","ninguno",550))
-inventario.push(new ropa("buzo","L","marron","dibujo",1200))
+inventario.push(new ropa("Collar Vendetta", "-", "negro","../assets/images/muestraAccesorios/imagenChica3.jpg", 900))
+inventario.push(new ropa("Collar Audaz", "-", "blanca y celeste","../assets/images/muestraAccesorios/imagenChica4.jpg", 800))
+inventario.push(new ropa("Pantalon Basic","40","negro","../assets/images/muestraHits/Hit2.jpeg",1400))
+inventario.push(new ropa("Blazer Exedra","S","naranja","../assets/images/muestraHits/Hit1.jpeg",550))
+inventario.push(new ropa("Pantalon de Gabardina","L","marron","../assets/images/muestraHits/Hit2.jpeg",1200))
 
-//funcion para que el usuario elija los items que quiere agregar al carrito
-function agregarAlCarrito(){
-    let carrito = []
-    for (let index = 0; index < inventario.length; index++) {
-        let agregarItem=prompt("desea agregar "+inventario[index].tipo+" al carrito? si | no")
-        if (agregarItem.toLowerCase()=="si"){
-            carrito.push(inventario[index])
-        }
+
+let productos = document.getElementById(`productos`)
+
+let item = ``
+let i
+showInventory()
+
+function showInventory(){
+    for (i=0;i<inventario.length;i++){
+
+        let column = document.createElement(`div`);
+        column.className = "col-md-6 col-xs-12 d-flex justify-content-center";
+
+            let nombreProducto = document.createElement(`h4`);
+            nombreProducto.className = `article__paragraph__fuente`;
+            nombreProducto.innerHTML = `${inventario[i].nombre}`;
+
+            let figure = document.createElement(`figure`)
+            figure.className = "figure figure__size__espacio"
+
+                let imagen = document.createElement(`img`);
+                imagen.className = `figure-img img-fluid rounded`;
+                imagen.src = `${inventario[i].imgSrc}`;
+
+                let figurePrecio = document.createElement(`figcaption`)
+                figurePrecio.className = `figure-caption text-right figure__CaptionBackground__color figure__figCaption__fuentes`
+                figurePrecio.innerHTML = `$ ${inventario[i].precio}`
+                
+                let figureTalle = document.createElement(`li`)
+                figureTalle.className = `article__paragraph__fuente`
+                figureTalle.innerHTML = `talle ${inventario[i].talle}`
+
+                let agregarCarritoBtn = document.createElement(`button`)
+                agregarCarritoBtn.className = `btn btn-outline-light`
+                agregarCarritoBtn.idName = `${i}`
+                agregarCarritoBtn.addEventListener(`click`,() =>{
+                    productoId = parseInt(agregarCarritoBtn.idName)
+                    agregarCarrito()
+                })
+                    let btnText = document.createElement(`h6`)
+                    btnText.className = `footer__links__fuente`
+                    btnText.innerHTML = `Agregar al carrito`
+
+                    agregarCarritoBtn.appendChild(btnText)
+
+                figure.appendChild(imagen)
+                figure.appendChild(figurePrecio)
+                figure.appendChild(figureTalle)
+                figure.appendChild(agregarCarritoBtn)
+            
+            column.appendChild(nombreProducto)
+            column.appendChild(figure)
+        
+        productos.appendChild(column)
+
     }
-    return carrito
 }
-//ejecucion de la funcion
-const carritoDeCompras = agregarAlCarrito()
-//prompt para dar la opcion de como sortear el array del carrito de compras en base al precio con las formulas abajo
-let orden = prompt("Quiere ver primero los items de mayor o menor precio? mayor | menor | vacio para no sortear")
 
-if (orden.toLowerCase() == "mayor"){
-    carritoDeCompras.sort((a,b) => {
-        return b.precio - a.precio;
-    });
-}
-else if (orden.toLowerCase() == "menor"){
-    carritoDeCompras.sort((a,b) => {
-        return a.precio - b.precio;
-    });
-}
-//output de los items a travez de un prompt uno por uno
-alert("estos son los items de tu carrito")
+let carrito = document.getElementById(`carrito`)
 
-for (const ropa of carritoDeCompras) {
-    alert("Item: "+ropa.tipo + "\n" +"talle: "+ ropa.talle+ "\n" +"color: "+ropa.color+ "\n" +"patron: "+ropa.patron+ "\n" +"precio: $"+ropa.precio)
-}
-//output de los items en lista
-for(const ropa of carritoDeCompras){
-    let item = document.createElement("li");
-    item.innerHTML = `<h3>producto: ${ropa.tipo}</h3
-                        <p>talle: ${ropa.talle}</p>
-                        <p>color: ${ropa.color}</p>
-                        <p>patron: ${ropa.patron}</p>
-                        <p>precio: ${ropa.precio}</p>
+function agregarCarrito(){
+    item += `
+        <div class="col-lg-3 article__title__color">
+            <ul>
+                <li class="footer__links__fuente">producto: ${inventario[productoId].nombre}</li>
+                <li>
+                    <figure style="width: 15%; height: 15%;">
+                        <img class="figure-img img-fluid rounded" src="${inventario[productoId].imgSrc}">
+                    </figure>
+                </li>
+                <li class="footer__links__fuente">talle: ${inventario[productoId].talle}</li>
+                <li class="footer__links__fuente">color: ${inventario[productoId].color}</li>
+                <li class="footer__links__fuente">precio: $ ${inventario[productoId].precio}</li>
+            </ul>
+        </div>
     `
-    const idUl = document.getElementById("carrito");
-    idUl.appendChild(item)
+
+
+    carrito.innerHTML = item;
 }
+
+// for(const ropa of carritoDeCompras){
+//     let item = document.createElement("li");
+//     item.innerHTML = `<h3>producto: ${ropa.nombre}</h3
+//                         <p>talle: ${ropa.talle}</p>
+//                         <p>color: ${ropa.color}</p>
+//                         <img class="height": ${ropa.imgSrc}>
+//                         <p>precio: ${ropa.precio}</p>
+//     `
+//     const idUl = document.getElementById("inventario");
+//     idUl.appendChild(item)
+// }
