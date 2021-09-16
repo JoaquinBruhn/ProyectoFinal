838dc0f2dca7917d390c7c8284344b25ab143b7d
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2021-09-15 22:43:20 -0300

    agruegue comentarios

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -4,8 +4,11 @@ const sets = []
 const extras = []
 const carrito = []
 
+//Este array no tiene un uso, esta por las dudas po si se necesita uno con todos los productos
 const inventario = []
-// constructor para maquetar los items
+
+
+// constructor para maquetar los items de la seccion de accesorios
 class accesorio{
     constructor(nombre,color,imgSrc,precio){
         this.nombre = nombre;
@@ -20,7 +23,7 @@ class accesorio{
 accesorios.push(new accesorio("Collar Vendetta", "dorado","../assets/images/muestraAccesorios/collar.jpeg", 900))
 accesorios.push(new accesorio("Collar Audaz", "dorado","../assets/images/muestraAccesorios/accesorio.jpg", 800))
 
-
+// constructor para maquetar los items de la seccion de sets
 class set{
     constructor(nombre,imgSrc,precio){
         this.nombre = nombre;
@@ -37,7 +40,7 @@ sets.push(new set("Basic","../assets/images/muestraSets/set3.jpg", 5500))
 sets.push(new set("Blue Love","../assets/images/muestraSets/set4.jpg", 5000))
 sets.push(new set("Cuatro Botones Blue","../assets/images/muestraSets/set5,5.jpg", 6000))
 
-
+// constructor para maquetar los items de la seccion de otros items sin categoria
 class extra{
     constructor(nombre,color,imgSrc,precio){
         this.nombre = nombre;
@@ -54,6 +57,8 @@ extras.push(new extra("Blusa Rose Negra", "negro","../assets/images/muestraSale/
 extras.push(new extra("Remeron", "blanco","../assets/images/muestraSale/imagenesMuestra/Remeron.jpeg", 1800))
 extras.push(new extra("Top", "negro","../assets/images/muestraSale/imagenesMuestra/Top.jpeg", 1700))
 
+
+//Los Arrays se meten en inventario 
 inventario.push(accesorios)
 inventario.push(sets)
 inventario.push(extras)
--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -1,5 +1,7 @@
+//AOS esta por las dudas por si se puede usar despues
 AOS.init();
 
+//se le ejecuta la funcion para armar el HTML a los 3 arrays, y el string va a servir para que el ID de los objetos sea unico
 mostrarProductos(accesorios, 'accesorios');
 mostrarProductos(sets, 'sets');
 mostrarProductos(extras, 'extras');
@@ -8,10 +10,12 @@ mostrarProductos(extras, 'extras');
 
 function mostrarProductos(productos, string) {
     for (let i = 0; i < productos.length; i++) {
+        //al product ID se le agrega el string que el FOR esta recorriendo en el momento y le da un numero unico para que el ID no se repita 
         productos[i].id = string + i
     }
     
     productos.forEach((producto) => {
+        //template para poner los items en el HTML, los ordena segun el array do donde salen
         $(`#productos-${string}`).append(`
             <div class="col-md-4">
             <h4 class="article__paragraph__fuente">${producto.nombre}</h4>
@@ -24,6 +28,7 @@ function mostrarProductos(productos, string) {
             <button type="button" id="${producto.id}" class="btn btn-outline-light"><h3 class="footer__links__fuente text-decoration-none m-0">Agregar al carrito</h3></button>
         </div>
         `);
+        //Se le agrega el evento de onClick a el boton
         $(`#${producto.id}`).click(function () {
             carrito.push(producto);
             console.log(carrito);
@@ -33,7 +38,9 @@ function mostrarProductos(productos, string) {
     });
 }
 
+//Funcion para agregar al carrito
 function mostrarCarrito(producto) {
+    //tamplate del item agregado
     $('#carrito').append(`
         <div class="col-md-4" id="removeCarrito${producto.id}">
             <h4 class="article__paragraph__fuente">${producto.nombre}</h4>
@@ -44,12 +51,14 @@ function mostrarCarrito(producto) {
             <button type="button" class="btn btn-outline-light" id="carrito${producto.id}"><h3 class="footer__links__fuente text-decoration-none m-0">Eliminar del carrito</h3></button>
         </div>
         `)
+        //Dandole un efecto de fadeIn
         $(`#removeCarrito${producto.id}`).hide()
         $(`#removeCarrito${producto.id}`).fadeIn(500)
+        //Evento para sacar el item del carrito
         $(`#carrito${producto.id}`).click(function () {
-            console.log(carrito.indexOf(producto));
             carrito.splice(carrito.indexOf(producto),1);
             console.log(carrito);
+            //Efecto fadeOut que elimina el item del HTML despues del fade
             $(`#removeCarrito${producto.id}`).fadeOut(500,()=>{
                 $(`#removeCarrito${producto.id}`).remove()
             })
